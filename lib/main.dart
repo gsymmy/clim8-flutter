b102101bdc60dc90a3c587fe96321d68aba712ac
Gursimran Singh <gsingh@gatech.edu>
2019-06-26 15:25:56 +0530

    Day 3

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,9 @@ import 'package:first_flutter/ui/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  var title = "Climate App";
   runApp(new MaterialApp(
-    title: "YouMatter",
-    home: Home(),
+    title: title,
+    home: Home(title: title),
   ));
 }
