391720b9da8e994758f3b78be8c0f7048dff35c8
Gursimran Singh <gsingh@gatech.edu>
2019-06-26 15:25:47 +0530

    Day 3

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,34 +1,41 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
+  final String title;
+
+  Home({Key key, this.title}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: AppBar(
-            actions: <Widget>[
-              new IconButton(
-                icon: new Icon(Icons.playlist_play),
-                onPressed: () => debugPrint("You Matter!"),
-              ),
-              new IconButton(
-                icon: new Icon(Icons.send),
-                onPressed: () => print("Search Res"),
-              )
-            ],
-            backgroundColor: Colors.deepOrangeAccent,
-            title: Text("YouMatter",
-                textDirection: TextDirection.ltr,
-                style: TextStyle(color: Colors.black, fontSize: 30))),
-        body: new Container(
-            alignment: Alignment.center,
-            child: new Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                new InkWell(
-                  child: new Text("Easter Egg!"),
-                  onTap: () => debugPrint("Voila!"),
-                )
-              ],
-            )));
+          title: new Text(title),
+          backgroundColor: Colors.black,
+        ),
+        body: new Center(
+          child: new CustomButton(),
+        ));
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new GestureDetector(
+        onTap: () {
+          final snackBar = new SnackBar(
+            content: new Text("Climate Change is really real!"),
+            backgroundColor: Theme.of(context).backgroundColor,
+            duration:
+                new Duration(hours: 0, minutes: 0, seconds: 1, microseconds: 0),
+          );
+          Scaffold.of(context).showSnackBar(snackBar);
+        },
+        child: new Container(
+            padding: new EdgeInsets.all(10.0),
+            decoration: new BoxDecoration(
+                color: Theme.of(context).buttonColor,
+                borderRadius: new BorderRadius.circular(5.5)),
+            child: new Text("For the Earth")));
   }
 }
