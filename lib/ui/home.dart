d0aafd0e68d8fd07d1476c047fd75ad9b4bc4774
Gursimran Singh <gsingh@gatech.edu>
2019-06-28 21:58:23 +0530

    Day 3

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -2,15 +2,23 @@ import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   final String title;
+
   Home({Key key, this.title}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(title),
-        backgroundColor: Colors.black12,
-      ),
-      floatingActionButton: new FloatingActionButton(),
-    );
+    return Scaffold(
+        appBar:
+            new AppBar(backgroundColor: Colors.blueAccent, title: new Text(title)),
+        body: Builder(builder: (BuildContext context) {
+          return new Center(
+              child: new IconButton(
+            icon: new Icon(Icons.arrow_drop_down_circle),
+            onPressed: () => Scaffold.of(context).showSnackBar(new SnackBar(
+                  content: new Text("Climate Change is Real"),
+                  backgroundColor: Theme.of(context).accentColor,
+                )),
+          ));
+        }));
   }
 }
