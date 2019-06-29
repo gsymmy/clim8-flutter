2d2a323c4bb51e57211a4ba81b09ecf8c2f550ba
Gursimran Singh <gsingh@gatech.edu>
2019-06-30 00:17:41 +0530

    Day 6

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -8,6 +8,11 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: new BottomNavigationBar(items: [
+        BottomNavigationBarItem(icon: new Icon(Icons.accessibility_new), title: new Text("Act")),
+        BottomNavigationBarItem(icon: new Icon(Icons.arrow_forward), title: new Text("Send")),
+        BottomNavigationBarItem(icon: new Icon(Icons.cloud_download), title: new Text("Download"))
+      ],),
         appBar:
             new AppBar(backgroundColor: Colors.blueAccent, title: new Text(title)),
         body: Builder(builder: (BuildContext context) {
@@ -20,5 +25,6 @@ class Home extends StatelessWidget {
                 )),
           ));
         }));
+
   }
 }
